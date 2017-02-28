a96f81c81014ecb1ad125711d088976e25831a96
wuguanyu <wuguanyu@makenv.com>
2017-02-28 16:32:24 +0800

    modify system.conf

---
+++ mc-core/src/main/java/com/makenv/model/mc/core/constant/Constant.java
@@ -0,0 +1,10 @@
+package com.makenv.model.mc.core.constant;
+
+/**
+ * Created by wgy on 2017/2/28.
+ */
+public class Constant {
+
+    //开始的脚本
+    public static final int START_HOUR = 18;
+}
---
+++ mc/src/main/java/com/makenv/model/mc/config/RootConfig.java
@@ -0,0 +1,7 @@
+package com.makenv.model.mc.config;
+
+/**
+ * Created by wgy on 2017/2/28.
+ */
+public class RootConfig {
+}
