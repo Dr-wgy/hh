36138cac866b6c5b650f119d0f5fe4bb7f44ee8c
wuguanyu <wuguanyu@makenv.com>
2017-03-02 17:19:44 +0800

    test griddesc

--- mc-core/src/main/java/com/makenv/model/mc/core/constant/Constant.java
+++ mc-core/src/main/java/com/makenv/model/mc/core/constant/Constant.java
@@ -5,11 +5,11 @@ package com.makenv.model.mc.core.constant;
  */
 public class Constant {
 
-  //开始的脚本
   public static final int START_HOUR = 18;
   public static final String[] FILE_HOURS = {"00", "06", "12", "18"};
   public static final int MODEL_DEBUG_LEVEL = 1;
   public static final String UNGRIB_RENV_FILE = "renv-ungrib.csh";
   public static final String UNGRIB_SCRIPT_FILE = "ungrib.sh";
   public static final String UNGRIB_FILE_PREFIX = "FILE";
+  public static final String COORDNAME = "China_d%02d";
 }
