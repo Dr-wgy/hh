c9b16a1cbde0b0a5c96f77b74705bdc82b0538be
alei <yangchunlei@makenv.com>
2017-03-02 14:48:04 +0800

    fix config

--- mc-core/src/main/java/com/makenv/model/mc/core/constant/Constant.java
+++ mc-core/src/main/java/com/makenv/model/mc/core/constant/Constant.java
@@ -9,4 +9,6 @@ public class Constant {
   public static final String UNGRIB_FILE_PREFIX = "FILE";
   public static final String[] FILE_HOURS = {"00", "06", "12", "18"};
   public static final int MODEL_DEBUG_LEVEL = 1;
+  public static final String UNGRIB_RENV_FILE = "renv-ungrib.csh";
+  public static final String UNGRIB_SCRIPT_FILE = "ungrib.csh";
 }
