2415441997e8aec2233d9ed60420eb92619cfcbc
alei <yangchunlei@makenv.com>
2017-03-09 17:20:34 +0800

    fix config & add wrf support

--- mc-core/src/main/java/com/makenv/model/mc/core/config/DomainRangeDir.java
+++ mc-core/src/main/java/com/makenv/model/mc/core/config/DomainRangeDir.java
@@ -4,25 +4,40 @@ package com.makenv.model.mc.core.config;
  * Created by wgy on 2017/2/28.
  */
 public class DomainRangeDir {
+  private String dirPath;
+  private UserCommonDir common;
+  private MissionDir missionid;
+  private String modelRunPath;
 
+  public String getModelRunPath() {
+    return modelRunPath;
+  }
 
-    private String dirPath;
+  public void setModelRunPath(String modelRunPath) {
+    this.modelRunPath = modelRunPath;
+  }
 
-    private UserCommonDir common;
+  public MissionDir getMissionid() {
+    return missionid;
+  }
 
-    public String getDirPath() {
-        return dirPath;
-    }
+  public void setMissionid(MissionDir missionid) {
+    this.missionid = missionid;
+  }
 
-    public void setDirPath(String dirPath) {
-        this.dirPath = dirPath;
-    }
+  public String getDirPath() {
+    return dirPath;
+  }
 
-    public UserCommonDir getCommon() {
-        return common;
-    }
+  public void setDirPath(String dirPath) {
+    this.dirPath = dirPath;
+  }
 
-    public void setCommon(UserCommonDir common) {
-        this.common = common;
-    }
+  public UserCommonDir getCommon() {
+    return common;
+  }
+
+  public void setCommon(UserCommonDir common) {
+    this.common = common;
+  }
 }
--- mc-core/src/main/java/com/makenv/model/mc/core/constant/Constant.java
+++ mc-core/src/main/java/com/makenv/model/mc/core/constant/Constant.java
@@ -5,7 +5,7 @@ package com.makenv.model.mc.core.constant;
  */
 public class Constant {
 
-//  public static final int START_HOUR = 18;
+  //  public static final int START_HOUR = 18;
   public static final String SYS_RENV_CSH = "sys_renv.csh";
   public static final String[] FILE_HOURS = {"00", "06", "12", "18"};
   public static final int MODEL_DEBUG_LEVEL = 1;
@@ -22,14 +22,17 @@ public class Constant {
   public static final String NAMELIST_WPS_METGRID_TEMPLATE = "namelist.wps.metgrid.template";
   public static final String NAMELIST_WPS_UNGRIB_TEMPLATE = "namelist.wps.ungrib.template";
 
-  public static final String MODULE_WRF_CSH = "Module.wrf.csh";
   public static final String GEOGRID_SCRIPT_FILE = "invoke-geogrid";
 
-  public static final String DOMAIN_RENV_FILE="renv-domain.csh";
+  public static final String DOMAIN_RENV_FILE = "renv-domain.csh";
 
   public static final String WRF_RENV_FILE = "renv-wrf";
-  public static final String WRF_SCRIPT_FILE = "invoke-wrf";
-  public static final String WRF_LOG_FILE = "log-wrf";
+  public static final String MODEL_SCRIPT_FILE = "invoke-model";
+  public static final String MODEL_LOG_FILE_INFO = "log-model-info";
+  public static final String MODEL_LOG_FILE_ERROR = "log-model-error";
 
   public static final String CSH_HEADER = "#!/bin/csh\n";
+
+  public static final String GLOBAL_TYPE_FNL = "fnl";
+  public static final String GLOBAL_TYPE_GFS = "gfs";
 }
