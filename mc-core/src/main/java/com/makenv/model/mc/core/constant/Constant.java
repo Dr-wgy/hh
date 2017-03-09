643d82f5fc978faa4211ff7b9d6b9cc6504c72ad
alei <yangchunlei@makenv.com>
2017-03-09 08:34:07 +0800

    fix config & change wrf task

--- mc-core/src/main/java/com/makenv/model/mc/core/constant/Constant.java
+++ mc-core/src/main/java/com/makenv/model/mc/core/constant/Constant.java
@@ -22,13 +22,11 @@ public class Constant {
   public static final String NAMELIST_WPS_UNGRIB_TEMPLATE = "namelist.wps.ungrib.template";
 
   public static final String MODULE_WRF_CSH = "Module.wrf.csh";
-  public static final String WRF_SCRIPT_FILE = "invoke-wrf";
   public static final String GEOGRID_SCRIPT_FILE = "invoke-geogrid";
 
-
   public static final String DOMAIN_RENV_FILE="renv-domain.sh";
-  public static final String WRF_RENV_FILE="renv-wrf.sh";
 
-
-
+  public static final String WRF_RENV_FILE = "renv-wrf";
+  public static final String WRF_SCRIPT_FILE = "invoke-wrf";
+  public static final String WRF_LOG_FILE = "log-wrf";
 }
