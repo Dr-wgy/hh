775e36f9ae23dd942b91b4a3348cfc0de1bfe828
wuguanyu <wuguanyu@makenv.com>
2017-03-13 19:54:39 +0800

    add mc-meic.json

---
+++ mc-meic/src/main/java/com/makenv/model/mc/meic/config/MeicServerParams.java
@@ -0,0 +1,7 @@
+package com.makenv.model.mc.meic.config;
+
+/**
+ * Created by wgy on 2017/3/13.
+ */
+public class MeicServerParams {
+}
---
+++ mc-meic/src/main/java/com/makenv/model/mc/meic/constants/Constant.java
@@ -0,0 +1,7 @@
+package com.makenv.model.mc.meic.constants;
+
+/**
+ * Created by wgy on 2017/3/13.
+ */
+public class Constant {
+}
