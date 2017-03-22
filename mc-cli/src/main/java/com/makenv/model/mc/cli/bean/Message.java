230b78fac56c71cdcbb24793b560c06564d3404a
wuguanyu <wuguanyu@makenv.com>
2017-03-22 09:11:49 +0800

    add timezone

--- mc-cli/src/main/java/com/makenv/model/mc/cli/bean/Message.java
+++ mc-cli/src/main/java/com/makenv/model/mc/cli/bean/Message.java
@@ -10,7 +10,7 @@ import java.util.Date;
 public class Message {
 
     private String id;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date time;
     private String type;
     private Object body;
