058d3119c9167a0a4e5cba4289232bb33ee6176a
wuguanyu <wuguanyu@makenv.com>
2017-03-22 15:42:46 +0800

    Revert "add timezone"
    This reverts commit 230b78fac56c71cdcbb24793b560c06564d3404a.

--- mc-cli/src/main/java/com/makenv/model/mc/cli/bean/Message.java
+++ mc-cli/src/main/java/com/makenv/model/mc/cli/bean/Message.java
@@ -10,7 +10,7 @@ import java.util.Date;
 public class Message {
 
     private String id;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date time;
     private String type;
     private Object body;
